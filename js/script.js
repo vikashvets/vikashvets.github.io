fc18b5687fc46ace03f922c8b8cac0b935568db2
vika shvets <vika.shvets1999@gmail.com>
2019-01-10 19:43:06 +0200

    HW-11

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
-$(document).on('click', '#openMenu', function () {
-    $(this).toggleClass('active');
-    $('.menu-list').toggleClass('open');
-});
+$(document).ready(function () {
+    $('a[href^="#_"]').on('click', function () {
+        var target = $($(this).attr("href"));
+        if (target.length > 0) {
+            var scroll = target.offset().top;
+            $('html,body').animate({scrollTop: scroll}, 800);
+            return false;
+        }
+    });
+})
 
-$(document).on('touchstart' && 'touchend', function (e) {
-    var container = $(".header__menu");
-    if (container.has(e.target).length === 0) {
-        $('#openMenu').removeClass('active');
-        $('.menu-list').removeClass('open');
+var fActive = '';
+
+function filterImage(image) {
+    if (fActive != image) {
+        $('.sort').filter('.' + image).slideDown();
+        $('.sort').filter(':not(.' + image + ')').slideUp();
+        fActive = image;
     }
+}
+
+$('.f-websites').click(function () {
+    filterImage('websites');
+});
+$('.f-logos').click(function () {
+    filterImage('logos');
+});
+$('.f-brochures').click(function () {
+    filterImage('brochures');
+});
+$('.f-all').click(function () {
+    filterImage('all');
 });
